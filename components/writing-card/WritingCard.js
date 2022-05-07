d1f422b7228c099766724c26657b9d6134f7dc40
Jack <jack.branthwaite@gmail.com>
2022-05-07 15:31:30 +1200

    Writing content now pulling/displaying from Prismic

--- components/writing-card/WritingCard.js
+++ components/writing-card/WritingCard.js
@@ -2,18 +2,19 @@ import React from "react";
 import s from "./WritingCard.module.css";
 import PropTypes from "prop-types";
 
-const WritingCard = ({ title, description }) => {
+const WritingCard = ({ content }) => {
+  console.log(content);
+
   return (
     <div className={s.WritingCardContainer}>
-      <h3 className={s.WritingCardTitle}>{title}</h3>
-      <p className={s.WritingCardDescription}>{description}</p>
+      <h3 className={s.WritingCardTitle}>{content.title[0].text}</h3>
+      <p className={s.WritingCardDescription}>{content.content[0].text}</p>
     </div>
   );
 };
 
 WritingCard.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
+  content: PropTypes.object,
 };
 
 export default WritingCard;
--- pages/writing.js
+++ pages/writing.js
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WritingCard from "../components/writing-card/WritingCard";
 import s from "../styles/Writing.module.css";
 
+import { createClient } from "../prismicio";
+
 const Writing = () => {
-  return (
-    <div className={s.WritingContainer}>
-      <div className={s.WritingContentContainer}>
-        <div className={s.WritingTitleContainer}>
-          <h2 className={s.WritingTitle}>writing</h2>
-        </div>
-        <div className={s.WritingContentContainer}>
-          <WritingCard
-            title="test title"
-            description="this is a test description to be able to make sure that the card will not look silly from the length of it."
-          />
+  const client = createClient();
+
+  const [posts, setPosts] = useState();
+
+  const fetchData = async () => {
+    return await client
+      .getAllByType("writing-pos")
+      .then((res) => setPosts(res));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
+
+  if (posts) {
+    return (
+      <div className={s.WritingContainer}>
+        <div className={s.WritingWrapperContainer}>
+          <div className={s.WritingTitleContainer}>
+            <h2 className={s.WritingTitle}>writing</h2>
+          </div>
+          <div className={s.WritingContentContainer}>
+            {posts.map((post, i) => {
+              return <WritingCard content={post.data} key={i} />;
+            })}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default Writing;
