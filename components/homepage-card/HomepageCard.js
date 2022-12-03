85f43f03bb73b5275c32235f1cd61f9322be3804
Jack Branthwaite <jack@formfunfunction.com>
2022-12-03 13:00:35 +1300

    Changes to the homepage

--- components/homepage-card/HomepageCard.js
+++ components/homepage-card/HomepageCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import s from "./HomepageCard.module.scss";
 import PropTypes from "prop-types";
-
 import BackgroundOne from "../../assets/images/mathias-p-r-reding-reffXHzUiF8-unsplash.jpg";
 import BackgroundTwo from "../../assets/images/matthew-woinarowicz--ZIfQqO1K1o-unsplash.jpg";
 import Link from "next/link";
@@ -24,7 +23,12 @@ const HomepageCard = ({ title }) => {
         }
       >
         <div className={s.HomepageCardTitleWrapper}>
-          <div className={s.ShapeWrapper}></div>
+          <div className={s.ShapeWrapper}>
+            <img
+              src={title === "writing" ? ShapeOne.src : ShapeTwo.src}
+              className={s.Image}
+            />
+          </div>
           <h2 className={s.HomepageCardTitle}>{title}</h2>
         </div>
       </a>
